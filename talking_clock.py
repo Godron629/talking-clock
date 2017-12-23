38f8a2360e981eab2e6407e3c3e2ebcf6e15be31
Gideon <gideonrich@hotmail.ca>
2017-12-22 21:30:34 -0700

    Classify

--- talking_clock.py
+++ talking_clock.py
@@ -3,140 +3,141 @@ import pyaudio
 import wave
 import datetime
 
-number_to_word = {
-    0: " twelve",
-    1: " one",
-    2: " two",
-    3: " three",
-    4: " four",
-    5: " five",
-    6: " six",
-    7: " seven",
-    8: " eight",
-    9: " nine",
-    10: " ten",
-    11: " eleven",
-    12: " twelve",
-    13: " thirteen",
-    14: " fourteen",
-    15: " fifteen",
-    16: " sixteen",
-    17: " seventeen",
-    18: " eighteen",
-    19: " nineteen",
-    20: " twenty",
-    30: " thirty",
-    40: " fourty",
-    50: " fifty",
-}
 
-tens_to_word = {
-    2: " twenty",
-    3: " thirty",
-    4: " fourty",
-    5: " fifty",
-}
+class TalkingClock(object):
 
-def talking_clock(time):
-    global number_to_word
-    global tens_to_word
+    number_to_word = {
+        0: " twelve",
+        1: " one",
+        2: " two",
+        3: " three",
+        4: " four",
+        5: " five",
+        6: " six",
+        7: " seven",
+        8: " eight",
+        9: " nine",
+        10: " ten",
+        11: " eleven",
+        12: " twelve",
+        13: " thirteen",
+        14: " fourteen",
+        15: " fifteen",
+        16: " sixteen",
+        17: " seventeen",
+        18: " eighteen",
+        19: " nineteen",
+        20: " twenty",
+        30: " thirty",
+        40: " fourty",
+        50: " fifty",
+    }
 
-    try:
-        hour, minutes = [int(x) for x in time.split(":")]
-    except ValueError: 
-        raise ValueError("Time not in format 'HH:MM'")
+    tens_to_word = {
+        2: " twenty",
+        3: " thirty",
+        4: " fourty",
+        5: " fifty",
+    }
 
-    output = "It's"
-    output += number_to_word[hour % 12]
+    def talking_clock(self, time):
+        try:
+            hour, minutes = [int(x) for x in time.split(":")]
+        except ValueError: 
+            raise ValueError("Time not in format 'HH:MM'")
 
-    period = " am" if (hour % 24 < 12) else " pm"
-    minutes = minutes % 60
+        output = "It's"
+        output += self.number_to_word[hour % 12]
 
-    if minutes == 0:
-        pass
-    elif minutes < 20:
-        output += (" oh" if minutes < 10 else "") + number_to_word[minutes]
-    else:
-        tens, ones = divmod(minutes, 10)
-        output += tens_to_word[tens]
+        period = " am" if (hour % 24 < 12) else " pm"
+        minutes = minutes % 60
 
-        if minutes % 10:
-            output += number_to_word[ones]
+        if minutes == 0:
+            pass
+        elif minutes < 20:
+            output += (" oh" if minutes < 10 else "") + self.number_to_word[minutes]
+        else:
+            tens, ones = divmod(minutes, 10)
+            output += self.tens_to_word[tens]
 
-    return output + period 
+            if minutes % 10:
+                output += self.number_to_word[ones]
 
-def time_to_speech(sentance):
-    global number_to_word
-    word_to_number = {v[1:]: str(k) for k, v in number_to_word.iteritems()}
+        return output + period 
 
-    directory = os.path.dirname(os.path.realpath(__file__))
+    def time_to_speech(self, sentance):
+        word_to_number = {v[1:]: str(k) for k, v in self.number_to_word.iteritems()}
 
-    sentance = sentance.split()
+        directory = os.path.dirname(os.path.realpath(__file__))
 
-    sounds_to_play = ["its"]
-    am_or_pm = sentance[-1]
+        sentance = sentance.split()
 
-    if len(sentance) == 3:
-        sounds_to_play.append(word_to_number[sentance[1]])
+        sounds_to_play = ["its"]
+        am_or_pm = sentance[-1]
 
-    elif len(sentance) == 4:
-        sounds_to_play.append(word_to_number[sentance[1]])
+        if len(sentance) == 3:
+            sounds_to_play.append(word_to_number[sentance[1]])
 
-        if sentance[2].endswith("ty"):
-            sounds_to_play.append(sentance[2].split("ty")[0])
-            sounds_to_play.append("ty")
+        elif len(sentance) == 4:
+            sounds_to_play.append(word_to_number[sentance[1]])
 
-        elif sentance[2].endswith("teen"):
-            sounds_to_play.append(sentance[2].split("teen")[0])
-            sounds_to_play.append("teen")
+            if sentance[2].endswith("ty"):
+                sounds_to_play.append(sentance[2].split("ty")[0])
+                sounds_to_play.append("ty")
 
-        else: 
-            sounds_to_play.append(word_to_number[sentance[2]])
-            
+            elif sentance[2].endswith("teen"):
+                sounds_to_play.append(sentance[2].split("teen")[0])
+                sounds_to_play.append("teen")
 
-    elif len(sentance) == 5:
-        sounds_to_play.append(word_to_number[sentance[1]])
+            else: 
+                sounds_to_play.append(word_to_number[sentance[2]])
+                
 
-        if sentance[2].endswith("ty"):
-            sounds_to_play.append(sentance[2].split("ty")[0])
-            sounds_to_play.append("ty")
+        elif len(sentance) == 5:
+            sounds_to_play.append(word_to_number[sentance[1]])
 
-        elif sentance[2] == "oh":
-            sounds_to_play.append("o")
+            if sentance[2].endswith("ty"):
+                sounds_to_play.append(sentance[2].split("ty")[0])
+                sounds_to_play.append("ty")
 
-        sounds_to_play.append(word_to_number[sentance[3]])
+            elif sentance[2] == "oh":
+                sounds_to_play.append("o")
 
-    sounds_to_play.append(am_or_pm)
+            sounds_to_play.append(word_to_number[sentance[3]])
 
-    for i in sounds_to_play:
-        play_sounds(os.path.join(directory, "sound_clips", i + ".wav"))
+        sounds_to_play.append(am_or_pm)
+
+        for i in sounds_to_play:
+            self.play_sounds(os.path.join(directory, "sound_clips", i + ".wav"))
 
 
-def play_sounds(sound_path):
-    chunk = 1024
-    f = wave.open(sound_path, "rb")
-    p = pyaudio.PyAudio()
+    @staticmethod
+    def play_sounds(sound_path):
+        chunk = 1024
+        f = wave.open(sound_path, "rb")
+        p = pyaudio.PyAudio()
 
-    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                    channels=f.getnchannels(),
-                    rate = f.getframerate(),
-                    output=True)
+        stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                        channels=f.getnchannels(),
+                        rate = f.getframerate(),
+                        output=True)
 
-    data = f.readframes(chunk)
-
-    while data: 
-        stream.write(data)
         data = f.readframes(chunk)
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+        while data: 
+            stream.write(data)
+            data = f.readframes(chunk)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
 
 if __name__ == '__main__':
-    times = ["13:00", "13:01", "13:10", "13:15", "13:20", "13:31", "04:55"]
-    for time in times:
-        sentance = talking_clock(time)
-        print sentance
-        time_to_speech(sentance)
+    talker = TalkingClock()
+
+    time = datetime.datetime.now().strftime("%H:%M")
+
+    sentance = talker.talking_clock(time)
+    talker.time_to_speech(sentance)
 
